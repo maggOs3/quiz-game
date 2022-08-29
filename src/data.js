9b81a793934b46c36f9f275818d4412635019763
Max <maxbelykh2001@gmail.com>
2022-08-29 13:59:42 +0300

    make data for quiz

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    id: 1,
+    category: "Science & Nature",
+    type: "multiple",
+    difficulty: "hard",
+    question:
+      "An organic compound is considered an alcohol if it has what functional group?",
+    correct_answer: "Hydroxyl",
+    incorrect_answers: ["Carbonyl", "Alkyl", "Aldehyde"],
+  },
+  {
+    id: 2,
+    category: "Entertainment: Music",
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "Pink Floyd made this song for their previous lead singer Syd Barrett.",
+    correct_answer: "Shine On You Crazy Diamond",
+    incorrect_answers: [
+      "Wish You Were Here",
+      "Have A Cigar",
+      "Welcome to the Machine",
+    ],
+  },
+  {
+    id: 3,
+    category: "Entertainment: Television",
+    type: "boolean",
+    difficulty: "medium",
+    question: "The television show Doctor Who first aired in 1963.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    id: 4,
+    category: "Entertainment: Music",
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "Which country does the electronic music duo &quot;The Knife&quot; originate from?",
+    correct_answer: "Sweden",
+    incorrect_answers: ["Finland", "Denmark", "Norway"],
+  },
+  {
+    id: 5,
+    category: "Science & Nature",
+    type: "multiple",
+    difficulty: "hard",
+    question: "What is the same in Celsius and Fahrenheit?",
+    correct_answer: "-40",
+    incorrect_answers: ["32", "-39", "-42"],
+  },
+];
