ba3632538f87fe11c0578ed0d26b571248de025d
Max <maxbelykh2001@gmail.com>
2022-08-29 16:43:18 +0300

    connect css file

--- src/components/welcome-page.js
+++ src/components/welcome-page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Welcome() {
   return (
-    <div>
+    <div className="main-container">
       <h1>Quizzical</h1>
       <p>Cool challenge for you</p>
       <button>Start quiz</button>
