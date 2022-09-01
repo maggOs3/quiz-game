cc7839826a290e1ec889bd29594179045e2b9e66
Max <maxbelykh2001@gmail.com>
2022-09-01 19:54:33 +0300

    make test resolution

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,14 +3,23 @@ import React from "react";
 export default function Quiz(props) {
   console.log(props);
   const incAnswArr = props.item.incorrect_answers; /*mb use state*/
+
+  const [z, zz] = React.useState(false);
+  console.log(z);
   const inAnsw = incAnswArr.map((answer) => {
-    return <button className="button-style">{answer}</button>;
+    return (
+      <button className="button-style" onClick={() => zz((fl) => false)}>
+        {answer}
+      </button>
+    );
   });
   return (
     <div className="q-and-var-container">
       <h3 className="question">{props.item.question}</h3>
       <div className="var-container">
-        <button className="button-style">{props.item.correct_answer}</button>
+        <button className="button-style" onClick={() => zz((tr) => true)}>
+          {props.item.correct_answer}
+        </button>
         {inAnsw}
       </div>
 
