517756822bf1eb1541cd6c681c3dc9c6b80e39d7
Max <maxbelykh2001@gmail.com>
2022-09-05 17:33:18 +0300

    upgrade logic

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,16 +4,35 @@ export default function Quiz(props) {
   console.log(props);
   const incAnswArr = props.item.incorrect_answers; /*mb use state*/
   const inAnsw = incAnswArr.map((answer) => {
-    return <button className="button-style">{answer}</button>;
+    return (
+      <button className="button-style" onClick={() => setCurAnsw(false)}>
+        {answer}
+      </button>
+    );
   });
+  const [currentAnsw, setCurAnsw] = React.useState(false);
+  // function toogleAnsw() {
+  //   setCurAnsw((oldAnsw) => !oldAnsw);
+  //   console.log(currentAnsw);
+  // }
+  let count = 0;
+  if (currentAnsw === true) {
+    count++;
+  } else if (currentAnsw === false) {
+    count--;
+  }
+  console.log(currentAnsw);
   return (
     <div className="q-and-var-container">
       <h3 className="question">{props.item.question}</h3>
       <div className="var-container">
-        <button className="button-style">{props.item.correct_answer}</button>
+        <button className="button-style" onClick={() => setCurAnsw(true)}>
+          {props.item.correct_answer}
+        </button>
         {inAnsw}
       </div>
-
+      {currentAnsw}
+      {count}
       {/* <hr></hr> */}
     </div>
   );
